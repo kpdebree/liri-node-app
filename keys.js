644c5ec63d1f8937248b958e85b94ad599142456
Kevin de Bree <kpdebree@gmail.com>
2017-10-29 22:30:43 -0700

    initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '4LpgX3Ga5OeSKxY4xHufbeCNI',
+  consumer_secret: 'G9clCN4my2NSALqVCKjfPkFQSew34CbZk14DK1IuFFI2VKhVBH',
+  access_token_key: '924770207012241408-ebeuTHIgpLAH7Ok872j4dnIUp5HjvKh',
+  access_token_secret: 'J6U4SasbbEWlxZx4NiSDIiq3so0QbYdd03jlyItrzdG7W',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,101 @@
+var twitter_keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+var Twitter = require('twitter');
+
+var command = process.argv[2];
+var command_string = process.argv[3];
+var command_extra = process.argv[4];
+
+var user_tweets = [];
+
+var spotify_keys = {
+	id: "c66acb2e924e42af951c060ae92ff2c8",
+	secret: "52bb4a1764c849b494a3c5606ee4f5bd"
+}
+
+const twitter = new Twitter(twitter_keys)
+const spotify = new Spotify(spotify_keys)
+
+
+function initialize() {
+	switch(command) {
+		case "my-tweets":
+			console.log("twitter confirmed")
+			getTweets();
+			break;
+		case "spotify-this-song":
+			getSpotify();
+			break;
+		case "movie-this":
+			getMovie();
+			break;
+		case "do-what-it-says":
+			readTxt();
+			break;
+		default:
+			console.log("Please Enter A Valid Command");
+	};
+};
+
+function getTweets() {
+	user_tweets = [];
+	var user_name = {screen_name: 'cfmonkey45'};
+	twitter.get('statuses/user_timeline', user_name, function(error, tweets, response) {
+		if (!error) {
+			console.log("TWITTER: Last 20 Tweets for " + user_name);
+			for (i = 0; i < 20; i++) {
+				user_tweets.push(tweets[i].text)
+				console.log(tweets[i].text);
+			}
+		} else if (error) {
+			console.log(error)
+		};
+	});
+
+};
+
+function getSpotify() {
+	if (command_string === undefined) {
+		command_string = "The Sign";
+		console.log("String Undefined. Initiating Default Search")
+	};
+	spotify.search({ type: 'track', query: command_string }, function(err, data) {
+		if (err) {
+			console.log(err)
+			return;
+		} else {
+			for (i = 0; i < data.tracks.items.length; i++) {
+				console.log("Song: " + data.tracks.items[i].name)
+				console.log("Artist: " +data.tracks.items[i].artists[0].name);
+        		console.log("Album: "+data.tracks.items[i].album.name);
+        		console.log("Link: "+data.tracks.items[i].href);
+				console.log("=====================")
+			};
+		};
+	});
+
+};
+
+function getMovie() {
+	if(command_string === undefined) {
+		command_string = "Mr. Nobody";
+	};
+	request('http://www.omdbapi.com/?t=' + command_string + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
+		if ( !error && response.statusCode == 200) {
+			var movie = JSON.parse(body);
+		    console.log("Title: " + movie.Title);
+		    console.log("Year: " + movie.Year);
+		    console.log("imdb Rating: " + movie.imdbRating);
+		    console.log("Country: " + movie.Country);
+		    console.log("Language: " + movie.Language);
+		    console.log("Plot: " + movie.Plot);
+		    console.log("Actors: " + movie.Actors);
+		    console.log("RottenTomatoes Rating: " + movie.tomatoRating);
+		    console.log("RottenTomatoes Link: " + movie.tomatoURL);
+		}
+	})
+}
+
+initialize();
